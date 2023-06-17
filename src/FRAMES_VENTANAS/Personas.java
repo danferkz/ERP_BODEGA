26582c1e311204552bf30409d6cd83b3cd9c97cf
AlvaroChN <vadrovezez@gmail.com>
2023-06-17 17:15:04 -0500

    waaaa

--- src/FRAMES_VENTANAS/Personas.java
+++ src/FRAMES_VENTANAS/Personas.java
@@ -12,6 +12,8 @@ import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Personas extends JFrame {//waaaa
 
@@ -67,28 +69,33 @@ Create the frame.*/
         Clientes.setLayout(null);
         
         JLabel lblNewLabel = new JLabel("Nombre: ");
-        lblNewLabel.setBounds(10, 11, 46, 14);
+        lblNewLabel.setBounds(46, 11, 46, 14);
         Clientes.add(lblNewLabel);
         
         txtNombreCliente = new JTextField();
-        txtNombreCliente.setBounds(66, 8, 86, 20);
+        txtNombreCliente.setBounds(102, 8, 86, 20);
         Clientes.add(txtNombreCliente);
         txtNombreCliente.setColumns(10);
         
         JButton btnBuscarCliente = new JButton("Buscar");
+        btnBuscarCliente.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		//Aqui va la fuincion buscar
+        	}
+        });
         btnBuscarCliente.setForeground(new Color(51, 0, 255));
         btnBuscarCliente.setFont(new Font("Tahoma", Font.BOLD, 13));
         btnBuscarCliente.setBackground(new Color(255, 255, 255));
-        btnBuscarCliente.setBounds(295, 6, 89, 23);
+        btnBuscarCliente.setBounds(329, 6, 89, 23);
         Clientes.add(btnBuscarCliente);
         
         JLabel lblNewLabel_2 = new JLabel("DNI:");
-        lblNewLabel_2.setBounds(162, 11, 46, 14);
+        lblNewLabel_2.setBounds(196, 11, 46, 14);
         Clientes.add(lblNewLabel_2);
         
         txtDNICliente = new JTextField();
         txtDNICliente.setColumns(10);
-        txtDNICliente.setBounds(191, 8, 86, 20);
+        txtDNICliente.setBounds(233, 8, 86, 20);
         Clientes.add(txtDNICliente);
         
         JScrollPane scrollPane = new JScrollPane();
