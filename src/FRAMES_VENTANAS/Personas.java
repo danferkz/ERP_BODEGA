0a38e7b3ec370a0c0666e00be8a387c26addc9cf
vaniavp <vaniavapa2015@hotmail.com>
2023-06-17 17:07:14 -0500

    hola

--- src/FRAMES_VENTANAS/Personas.java
+++ src/FRAMES_VENTANAS/Personas.java
@@ -9,12 +9,15 @@ import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 
 public class Personas extends JFrame {
 
     private JPanel contentPane;
     private JTextField txtNombreProv;
     private JTextField txtNombreClin;
+    private JTextField textField;
+    private JTextField textField_1;
 
     /*
      
@@ -46,17 +49,51 @@ Create the frame.*/
         contentPane.add(tabbedPane);
 
         JPanel Proveedor = new JPanel();
-        tabbedPane.addTab("Proveedor", null, Proveedor, null);
+        tabbedPane.addTab("Proveedores", null, Proveedor, null);
         Proveedor.setLayout(null);
         
-        JLabel lblNewLabel_1 = new JLabel("Nombre: ");
-        lblNewLabel_1.setBounds(10, 11, 46, 14);
-        Proveedor.add(lblNewLabel_1);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(214, 2, 213, 162);
+        Proveedor.add(scrollPane);
+        
+        JButton btnAgregar = new JButton("Agregar");
+        btnAgregar.setBounds(14, 176, 117, 29);
+        Proveedor.add(btnAgregar);
+        
+        JButton btnListar = new JButton("Listar");
+        btnListar.setBounds(150, 174, 117, 29);
+        Proveedor.add(btnListar);
+        
+        JButton btnEliminar = new JButton("Eliminar");
+        btnEliminar.setBounds(291, 174, 117, 29);
+        Proveedor.add(btnEliminar);
+        
+        JLabel lblNombre = new JLabel("Nombre: ");
+        lblNombre.setBounds(10, 11, 98, 14);
+        Proveedor.add(lblNombre);
         
         txtNombreProv = new JTextField();
-        txtNombreProv.setBounds(70, 8, 86, 20);
+        txtNombreProv.setBounds(92, 8, 112, 20);
         Proveedor.add(txtNombreProv);
         txtNombreProv.setColumns(10);
+        
+        JLabel lblDirección = new JLabel("Dirección:");
+        lblDirección.setBounds(10, 44, 70, 16);
+        Proveedor.add(lblDirección);
+        
+        JLabel lblRUC = new JLabel("RUC:");
+        lblRUC.setBounds(11, 76, 61, 16);
+        Proveedor.add(lblRUC);
+        
+        textField = new JTextField();
+        textField.setBounds(92, 40, 110, 26);
+        Proveedor.add(textField);
+        textField.setColumns(10);
+        
+        textField_1 = new JTextField();
+        textField_1.setBounds(91, 78, 116, 26);
+        Proveedor.add(textField_1);
+        textField_1.setColumns(10);
 
         JPanel Clientes = new JPanel();
         tabbedPane.addTab("Clientes", null, Clientes, null);
